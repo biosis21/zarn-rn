cea49bc2639dd1d0ac8560815e97d87473d0cb05
mytrofanov <mytrofanovmaxym@gmail.com>
2023-06-23 10:01:54 +0300

    feat: log for appdelegate

--- ios/zarn/AppDelegate.mm
+++ ios/zarn/AppDelegate.mm
@@ -40,5 +40,16 @@
   return [ShareMenuManager application:app openURL:url options:options];
 }
 
+/// log for checking recieved data from extention:
+- (BOOL)application:(UIApplication *)app
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options
+{
+  NSLog(@"AppDelegate openURL called with URL: %@", url);
+  NSLog(@"Options: %@", options);
+
+  return [ShareMenuManager application:app openURL:url options:options];
+}
+
 @end
 
